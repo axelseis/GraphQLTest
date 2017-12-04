f6b161f50d2e39860b2a40eb4abcbf4e00671b21
Axel Claver <AxelClaver@dorna.com>
2017-12-04 11:59:32 +0100

    fix(Init): remove old data import

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { jsonGraphqlExpress } from 'json-graphql-server';
-import championship from './riders.js';
 import data from './ridersDB.js';
 
 const PORT = 3000;

55f1c2c7d6d649ea06942b1201650df357eccf53
Axel Claver <AxelClaver@dorna.com>
2017-12-04 13:31:55 +0100

    fix(Server): Add cors policies

--- server/index.js
+++ server/index.js
@@ -5,6 +5,20 @@ import data from './ridersDB.js';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+})
+
 app.use('/graphql', jsonGraphqlExpress(data));
 app.listen(PORT, () => {
  console.log("Server started at port: ", PORT);

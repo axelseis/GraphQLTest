c7fcaad0b4725a3f5dcb00271641b343b0befb9b
Axel Claver <AxelClaver@dorna.com>
2017-12-04 11:47:12 +0100

    feat(Init): add ridersDB json

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { jsonGraphqlExpress } from 'json-graphql-server';
 import championship from './riders.js';
+import data from './ridersDB.js';
 
 const PORT = 3000;
 const app = express();
 
-app.use('/graphql', jsonGraphqlExpress(championship));
+app.use('/graphql', jsonGraphqlExpress(data));
 app.listen(PORT);

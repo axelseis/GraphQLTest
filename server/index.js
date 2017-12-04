303e4274b4cb40f5cbc659df78dd765b9397bf06
Axel Claver <AxelClaver@dorna.com>
2017-12-04 12:57:18 +0100

    fix(Index): add env.port variable

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import { jsonGraphqlExpress } from 'json-graphql-server';
 import data from './ridersDB.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use('/graphql', jsonGraphqlExpress(data));
-app.listen(PORT);
+app.listen(PORT, () => {
+ console.log("Server started at port: ", PORT);
+});
